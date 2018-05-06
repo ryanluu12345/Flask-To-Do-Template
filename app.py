19a9fc65e82889bdcd1ce4ede69c70b7d08db1e8
Ryan Luu <ryanluu12345@gmail.com>
2018-05-05 17:18:17 -0700

    First commit

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app=Flask(__name__)
 '''Allows users to go to the index page'''
 @app.route("/")
 def index():
-    #TODO:(4.5) Show different template renderings using name
+    #TODO:(4) Show different template renderings using name
     name="Ryan"
     return render_template("index.html",name=name)
 
@@ -41,9 +41,9 @@ def signup():
 @app.route("/entry")
 def entry():
     #TODO:(2) Return the correct template
-    return render_template("entry.html")
+    return
 
-#TODO:(4) Render the correct template for the "home" page
+#TODO:(4) Render the correct template for the "home" page with tasks=tasks and name=name
 #TODO:(12) Add permissions for post requests
 @app.route("/home",methods=["GET","POST"])
 def home():
@@ -53,33 +53,13 @@ def home():
     name=""
 
     # TODO:(22) Update the tasks with the ones from the "tasks.json" file
-    #Opens the "tasks.json" file and reads the contents into tasks
-    with open("tasks.json", "r") as f:
-        tasks = json.load(f)
 
     #TODO:(13) Check post request
     #TODO:(14) Get the entered username
     #TODO:(15) Check if the username and password match
     #TODO:(16) If there is a match, re-render the "home" page with the tasks and the username
 
-    #Checks the form method
-    if request.method=="POST":
-
-        #Obtains username and password from the form
-        username=request.form["username"]
-        password=request.form["password"]
-
-        #Loads the user login data and compares with the posted data
-        with open("user.json","r") as f:
-            user_data=json.load(f)
-
-            #Checks to see if the entered usernames and passwords match with the record in "user.json"
-            if user_data["username"]==username and user_data["password"]==password:
-
-                #Re-renders template with user data
-                return render_template("home.html",tasks=tasks,name=username)
-
-    return render_template("home.html",tasks=tasks,name=name)
+    return
 
 #TODO:(17) Check post request
 #TODO:(18) Create an empty list for tasks
@@ -88,63 +68,18 @@ def home():
 #TODO:(21) Write all of the tasks to the "tasks.json" file
 @app.route("/confirm-entry",methods=["POST","GET"])
 def confirm_entry():
-
-    #Checks if the method of the request is post
-    if request.method=="POST":
-
-        #Initializes an empty list
-        tasks_list=[]
-
-        #Obtains the tasks from the frontend form
-        tasks=request.form
-
-        #Adds each task to the tasks_list
-        for key in tasks:
-            task=tasks[key]
-            tasks_list.append(task)
-
-        #Writes the tasks to the json file
-        with open("tasks.json","w") as f:
-            json.dump(tasks_list,f)
-
     return "Your tasks have been saved <br> <a href='home'><button>View Tasks</button></a>"
 
 #TODO:(1) Make a new route whose name is "confirm-signup"
 #TODO:(3) Make the new route render the "confirm-signup" page
 #TODO:(6) Allow post and get for the confirmation page
-@app.route("/confirm-signup",methods=["POST","GET"])
-def confirm_signup():
 
+    '''Inside method'''
     #TODO:(7) Check post request form data
     #TODO:(8) Create variables from the posted data
     #TODO:(9) Store data in the "user" dictionary
     #TODO:(10) Store data in json file
     #TODO:(11) Server side redirect to "login" page
-
-    #Checks if the request method is post
-    if request.method=="POST":
-
-        #Initializes empty user dictionary
-        user={}
-
-        #Obtains the username, password, and email from the frontend form
-        username=request.form["username"]
-        password=request.form["password"]
-        email=request.form["email"]
-
-        #Stores the user data into the previously created user dictionary
-        user["password"]=password
-        user["username"]=username
-        user["email"]=email
-
-        #Writes the data to the user json file
-        with open("user.json","w") as f:
-            json.dump(user,f)
-
-        #Redirects to the login page
-        return redirect(url_for("login"))
-
-    return render_template("confirm-signup.html")
 
 if __name__=="__main__":
     app.run(debug=True) #Never run as debug=True in a production environment
